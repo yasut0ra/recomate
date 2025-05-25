2a01cca4d388e2c97c4b8e56c90629eef2db1ac7
yasut0ra <yastar.tkm83@gmail.com>
2025-05-25 17:38:14 +0900

    Electronアプリケーションのメインプロセスを実装する新しいファイルを追加。ウィンドウの作成とViteの開発サーバーへの接続を設定。

---
+++ ui/electron-main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // Viteの開発サーバーに接続
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
